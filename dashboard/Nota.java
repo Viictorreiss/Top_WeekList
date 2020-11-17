be1d9bdbae53204709e85ca2dde3a5bff88a6cc9
ana komase <elizakomase@gmail.com>
2020-11-16 22:52:22 -0300

    telas dashboard

---
+++ dashboard/Nota.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.dashboard;
+
+/**
+ *
+ * @author Ana Komase
+ */
+class Nota {
+    private double valor;
+    private Genero nomeGenero;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void nota(double nota, Genero nomeGenero){
+        this.valor = valor;
+    }
+
+    Object getGenero() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
